1ef43e2d9d50bce14bbc4be11f26797b9b8065a1
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-15 18:39:44 +0100

    add logging for elements with class 'playground' in public script

--- public/js/public-script.js
+++ public/js/public-script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("PUBLIC JS READY");
+  let elements = document.getElementsByClassName("playground");
+  //   for (let i = 0; i < elements.length; i++) {
+  //     console.log(elements[i].innerHTML);
+  //   }
+  arrElements = Array.from(elements);
+  numberOfElements = arrElements.length;
+  console.log(`${numberOfElements} element${numberOfElements > 1 ? "s" : ""} found!`);
+  Array.from(elements).forEach((element) => {
+    console.log(element.innerHTML);
+  });
 });
