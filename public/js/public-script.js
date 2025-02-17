71a18a6bb39a0e0519caab2aec27bed569e1cc08
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-17 16:35:25 +0100

    basic input validation

--- public/js/public-script.js
+++ public/js/public-script.js
@@ -17,45 +17,66 @@ document.addEventListener("DOMContentLoaded", function () {
   //   });
   //   console.log(elements);
   if (playground) {
-    let newCode = document.createElement("code");
-    newCode.textContent = "This is a code tag coming from my plugin.";
-    playground.appendChild(newCode);
-    let newParagraph = document.createElement("p");
-    newParagraph.textContent = "This is a paragraph tag coming from my plugin.";
-    newParagraph.style.color = "pink";
-    newParagraph.style.fontSize = "xx-large";
-    newParagraph.style.backgroundColor = "blue";
-    playground.appendChild(newParagraph);
-    let newButton = document.createElement("button");
-    const btnFunction = () => {
-      alert("Good job!");
-    };
-    newButton.textContent = "Click me!";
-    // newButton.onclick = btnFunction;
-    newButton.addEventListener("click", btnFunction);
-    playground.appendChild(newButton);
-    let mouseOverDiv = document.createElement("div");
-    mouseOverDiv.textContent = "Mouse over me!";
-    mouseOverDiv.style.padding = "10px";
-    mouseOverDiv.style.border = "1px solid black";
-    mouseOverDiv.style.backgroundColor = "yellow";
-    mouseOverDiv.addEventListener("mouseover", () => {
-      mouseOverDiv.style.backgroundColor = "red";
+    // let newCode = document.createElement("code");
+    // newCode.textContent = "This is a code tag coming from my plugin.";
+    // playground.appendChild(newCode);
+    // let newParagraph = document.createElement("p");
+    // newParagraph.textContent = "This is a paragraph tag coming from my plugin.";
+    // newParagraph.style.color = "pink";
+    // newParagraph.style.fontSize = "xx-large";
+    // newParagraph.style.backgroundColor = "blue";
+    // playground.appendChild(newParagraph);
+    // let newButton = document.createElement("button");
+    // const btnFunction = () => {
+    //   alert("Good job!");
+    // };
+    // newButton.textContent = "Click me!";
+    // // newButton.onclick = btnFunction;
+    // newButton.addEventListener("click", btnFunction);
+    // playground.appendChild(newButton);
+    // let mouseOverDiv = document.createElement("div");
+    // mouseOverDiv.textContent = "Mouse over me!";
+    // mouseOverDiv.style.padding = "10px";
+    // mouseOverDiv.style.border = "1px solid black";
+    // mouseOverDiv.style.backgroundColor = "yellow";
+    // mouseOverDiv.addEventListener("mouseover", () => {
+    //   mouseOverDiv.style.backgroundColor = "red";
+    // });
+    // mouseOverDiv.addEventListener("mouseleave", () => {
+    //   mouseOverDiv.style.backgroundColor = "yellow";
+    // });
+    // playground.appendChild(mouseOverDiv);
+    // let boringTextNode = document.createTextNode("Boring...");
+    // playground.appendChild(boringTextNode);
+    // let boringH3 = document.createElement("h3");
+    // playground.appendChild(boringH3);
+    // playground.removeChild(boringH3);
+    // let anotherP = document.createElement("p");
+    // anotherP.textContent = "This is another paragraph.";
+    // playground.appendChild(anotherP);
+    // pS = document.getElementsByTagName("p");
+    // console.log(pS);
+    const formInput = document.createElement("input");
+    formInput.setAttribute("type", "number");
+    formInput.setAttribute("min", "50");
+    formInput.setAttribute("max", "100");
+    formInput.setAttribute("id", "age");
+    playground.appendChild(formInput);
+    const message = document.createElement("p");
+    playground.appendChild(message);
+    const formBtn = document.createElement("button");
+    formBtn.textContent = "Check input";
+    formBtn.setAttribute("type", "button");
+    formBtn.addEventListener("click", () => {
+      if (formInput.checkValidity()) {
+        message.textContent = "✅ Input is valid!";
+        message.style.color = "green";
+      } else {
+        message.textContent = "❌ Input is invalid!";
+        message.style.color = "red";
+      }
     });
-    mouseOverDiv.addEventListener("mouseleave", () => {
-      mouseOverDiv.style.backgroundColor = "yellow";
-    });
-    playground.appendChild(mouseOverDiv);
-    let boringTextNode = document.createTextNode("Boring...");
-    playground.appendChild(boringTextNode);
-    let boringH3 = document.createElement("h3");
-    playground.appendChild(boringH3);
-    playground.removeChild(boringH3);
-    let anotherP = document.createElement("p");
-    anotherP.textContent = "This is another paragraph.";
-    playground.appendChild(anotherP);
-    pS = document.getElementsByTagName("p");
-    console.log(pS);
+    playground.appendChild(formBtn);
   } else {
     console.log("No playground found!");
   }
