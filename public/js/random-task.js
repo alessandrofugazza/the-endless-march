01f218bc667f4bce8e4ea75a101adb32d59b871a
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-17 12:50:57 +0100

    add task counter functionality to random task generator

--- public/js/random-task.js
+++ public/js/random-task.js
@@ -1,13 +1,23 @@
 const generatorPanel = document.querySelector(".generator-panel");
 if (generatorPanel) {
-  const btn = generatorPanel.querySelector(".wp-element-button");
+  let taskCounter = 0;
+  const generateTaskBtn = generatorPanel.querySelector(".generate-task-btn");
+  const resetTaskCounterBtn = generatorPanel.querySelector(".reset-counter-btn");
   const taskList = generatorPanel.querySelector(".quick-tasks");
   const tasks = taskList.querySelectorAll("li");
-  btn.addEventListener("click", () => {
+  const resultParagraph = generatorPanel.querySelector(".random-task");
+  //   TODO better spacing you amateur
+  const taskCounterParagraph = generatorPanel.querySelector(".task-counter");
+  generateTaskBtn.addEventListener("click", () => {
     const randomIndex = Math.floor(Math.random() * tasks.length);
     let randomTask = tasks[randomIndex].textContent.trim();
-    const resultParagraph = generatorPanel.querySelector(".random-task");
     resultParagraph.textContent = randomTask;
+    taskCounter += 1;
+    taskCounterParagraph.textContent = taskCounter;
+  });
+  resetTaskCounterBtn.addEventListener("click", () => {
+    taskCounter = 0;
+    taskCounterParagraph.textContent = taskCounter;
   });
 } else {
   console.log("No generator panel found!");
