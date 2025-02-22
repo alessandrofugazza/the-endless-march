1cc3a8cb1f32057b78984f03e65e927503833e83
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-22 16:52:35 +0100

    class-the-endless-march

--- includes/class-the-endless-march.php
+++ includes/class-the-endless-march.php
@@ -36,7 +36,6 @@ class The_Endless_March
      *
      * @since    0.1.2
      * @access   protected
-     * ALSO HERE GODDAMNIT
      * @var      The_Endless_March_Loader    $loader    Maintains and registers all hooks for the plugin.
      */
     protected $loader;
@@ -44,7 +43,7 @@ class The_Endless_March
     /**
      * The unique identifier of this plugin.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      * @access   protected
      * @var      string    $plugin_name    The string used to uniquely identify this plugin.
      */
@@ -53,7 +52,7 @@ class The_Endless_March
     /**
      * The current version of the plugin.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      * @access   protected
      * @var      string    $version    The current version of the plugin.
      */
@@ -66,16 +65,16 @@ class The_Endless_March
      * Load the dependencies, define the locale, and set the hooks for the admin area and
      * the public-facing side of the site.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      */
     public function __construct()
     {
-        if (defined('PLUGIN_NAME_VERSION')) {
-            $this->version = PLUGIN_NAME_VERSION;
+        if (defined('THE_ENDLESS_MARCH_VERSION')) {
+            $this->version = THE_ENDLESS_MARCH_VERSION;
         } else {
-            $this->version = '1.0.0';
+            $this->version = '0.1.2';
         }
-        $this->plugin_name = 'plugin-name';
+        $this->plugin_name = 'the-endless-march';
 
         $this->load_dependencies();
         $this->set_locale();
@@ -97,7 +96,7 @@ class The_Endless_March
      * Create an instance of the loader which will be used to register the hooks
      * with WordPress.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      * @access   private
      */
     private function load_dependencies()
@@ -107,26 +106,26 @@ class The_Endless_March
          * The class responsible for orchestrating the actions and filters of the
          * core plugin.
          */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-plugin-name-loader.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-the-endless-march-loader.php';
 
         /**
          * The class responsible for defining internationalization functionality
          * of the plugin.
          */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-plugin-name-i18n.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-the-endless-march-i18n.php';
 
         /**
          * The class responsible for defining all actions that occur in the admin area.
          */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-plugin-name-admin.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-the-endless-march-admin.php';
 
         /**
          * The class responsible for defining all actions that occur in the public-facing
          * side of the site.
          */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-plugin-name-public.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-the-endless-march-public.php';
 
-        $this->loader = new Plugin_Name_Loader();
+        $this->loader = new The_Endless_March_Loader();
 
     }
 
@@ -136,13 +135,13 @@ class The_Endless_March
      * Uses the Plugin_Name_i18n class in order to set the domain and to register the hook
      * with WordPress.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      * @access   private
      */
     private function set_locale()
     {
 
-        $plugin_i18n = new Plugin_Name_i18n();
+        $plugin_i18n = new The_Endless_March_i18n();
 
         $this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
 
@@ -152,13 +151,13 @@ class The_Endless_March
      * Register all of the hooks related to the admin area functionality
      * of the plugin.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      * @access   private
      */
     private function define_admin_hooks()
     {
 
-        $plugin_admin = new Plugin_Name_Admin($this->get_plugin_name(), $this->get_version());
+        $plugin_admin = new The_Endless_March_Admin($this->get_plugin_name(), $this->get_version());
 
         $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
         $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
@@ -169,13 +168,13 @@ class The_Endless_March
      * Register all of the hooks related to the public-facing functionality
      * of the plugin.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      * @access   private
      */
     private function define_public_hooks()
     {
 
-        $plugin_public = new Plugin_Name_Public($this->get_plugin_name(), $this->get_version());
+        $plugin_public = new The_Endless_March_Public($this->get_plugin_name(), $this->get_version());
 
         $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
         $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
@@ -185,7 +184,7 @@ class The_Endless_March
     /**
      * Run the loader to execute all of the hooks with WordPress.
      *
-     * @since    1.0.0
+     * @since    0.1.2
      */
     public function run()
     {
@@ -196,8 +195,8 @@ class The_Endless_March
      * The name of the plugin used to uniquely identify it within the context of
      * WordPress and to define internationalization functionality.
      *
-     * @since     1.0.0
-     * @return    string    The name of the plugin.
+     * @since     0.1.2
+     * @return    string    The Endless March
      */
     public function get_plugin_name()
     {
@@ -207,8 +206,8 @@ class The_Endless_March
     /**
      * The reference to the class that orchestrates the hooks with the plugin.
      *
-     * @since     1.0.0
-     * @return    Plugin_Name_Loader    Orchestrates the hooks of the plugin.
+     * @since     0.1.2
+     * @return    The_Endless_March_Loader    Orchestrates the hooks of the plugin.
      */
     public function get_loader()
     {
@@ -218,8 +217,8 @@ class The_Endless_March
     /**
      * Retrieve the version number of the plugin.
      *
-     * @since     1.0.0
-     * @return    string    The version number of the plugin.
+     * @since     0.1.2
+     * @return    string    0.1.2
      */
     public function get_version()
     {
